41beb1c4144bba7dcc8fa4f3fc971198c2d70b6b
yeasin habib <yeasinhabib1994@gmail.com>
2018-09-16 21:50:39 +0600

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            PhotoProcessor.PhotoFilterHnadler filterHnadler = filters.ApplyBrightness;
+            filterHnadler += filters.ApplyContrast;
+            filterHnadler += filters.RemoveRedEyeFilter;
+            processor.Process("photo.jpg", filterHnadler);
+        }
+    }
+
+    public class Photo
+    {
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("photo is saved");
+        }
+    }
+
+    public class PhotoProcessor
+    {
+        public delegate void PhotoFilterHnadler(Photo photo);
+
+        public void Process(string path, PhotoFilterHnadler filterHnadler)
+        {
+            var photo = Photo.Load(path);
+
+            filterHnadler(photo);
+
+            photo.Save();
+        }
+    }
+
+    public class PhotoFilters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("brightness is applied");
+        }
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Contrast is applied");
+        }
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("photo is resized");
+        }
+        public void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Red eye is remove");
+        }
+    }
+}
